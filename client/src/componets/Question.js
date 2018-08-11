5b11bb28f08212af67c1f4a7695a1c03434178b6
kennf136 <kennf136@gmail.com>
2018-08-10 21:28:05 -0400

    trying to get questions inside componet

---
+++ client/src/componets/Question.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Questions extends Component {
+    render() {
+      console.log(this.props)
+        return (
+            <div>
+                
+                    {/* <div> {this.props.copy}</div> */}
+                 
+            </div>
+
+       
+        );
+    }
+}
+
+export default Questions;
--- client/src/componets/api.js
+++ client/src/componets/api.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
+import Questions from './Question';
 
 class Api extends Component {
 
@@ -39,21 +40,26 @@ class Api extends Component {
             const easyQuestionAReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=easy&type=multiple`)
             const mediumQuestionAReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=medium&type=multiple`)
             const HardQuestionAReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=29&difficulty=hard&type=multiple`)
-            const easyQuestionBReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=easy&type=multiple`)
-            const mediumQuestionBReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=medium&type=multiple`)
-            const HardQuestionBReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=29&difficulty=hard&type=multiple`)
-            const easyQuestionCReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=easy&type=multiple`)
-            const mediumQuestionCReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=medium&type=multiple`)
-            const HardQuestionCReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=29&difficulty=hard&type=multiple`)
-            const easyQuestionDReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=easy&type=multiple`)
-            const mediumQuestionDReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=medium&type=multiple`)
-            const HardQuestionDReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=29&difficulty=hard&type=multiple`)
-            const easyQuestionEReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=easy&type=multiple`)
-            const mediumQuestionEReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=medium&type=multiple`)
-            const HardQuestionEReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=29&difficulty=hard&type=multiple`)
-            const easyQuestionFReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=easy&type=multiple`)
-            const mediumQuestionFReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=29&difficulty=medium&type=multiple`)
-            const HardQuestionFReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=29&difficulty=hard&type=multiple`)
+
+            const easyQuestionBReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=31&difficulty=easy&type=multiple`)
+            const mediumQuestionBReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=31&difficulty=medium&type=multiple`)
+            const HardQuestionBReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=31&difficulty=hard&type=multiple`)
+
+            const easyQuestionCReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=32&difficulty=easy&type=multiple`)
+            const mediumQuestionCReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=32&difficulty=medium&type=multiple`)
+            const HardQuestionCReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=32&difficulty=hard&type=multiple`)
+
+            const easyQuestionDReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=18&difficulty=easy&type=multiple`)
+            const mediumQuestionDReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=18&difficulty=medium&type=multiple`)
+            const HardQuestionDReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=18&difficulty=hard&type=multiple`)
+
+            const easyQuestionEReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=15&difficulty=easy&type=multiple`)
+            const mediumQuestionEReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=15&difficulty=medium&type=multiple`)
+            const HardQuestionEReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=15&difficulty=hard&type=multiple`)
+
+            const easyQuestionFReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=12&difficulty=easy&type=multiple`)
+            const mediumQuestionFReponse = await axios.get(`https://opentdb.com/api.php?amount=2&category=12&difficulty=medium&type=multiple`)
+            const HardQuestionFReponse = await axios.get(`https://opentdb.com/api.php?amount=1&category=12&difficulty=hard&type=multiple`)
           
             this.setState({
                 // category:[{
@@ -84,14 +90,54 @@ class Api extends Component {
 
     render() {
 
-
-
+const cata= this.state.hardA.map((cat)=>{
+    return(
+        <div>
+          <h1>  {cat.category} </h1> 
+        </div>
+    )
+})
+const catb= this.state.hardB.map((cat)=>{
+    return(
+        <div>
+          <h1>  {cat.category} </h1> 
+        </div>
+    )
+})
+const catc= this.state.hardC.map((cat)=>{
+    return(
+        <div>
+          <h1>  {cat.category} </h1> 
+        </div>
+    )
+})
+const catd= this.state.hardD.map((cat)=>{
+    return(
+        <div>
+          <h1>  {cat.category} </h1> 
+        </div>
+    )
+})
+const cate= this.state.hardE.map((cat)=>{
+    return(
+        <div>
+          <h1>  {cat.category} </h1> 
+        </div>
+    )
+})
+const catf= this.state.hardF.map((cat)=>{
+    return(
+        <div>
+          <h1>  {cat.category} </h1> 
+        </div>
+    )
+})
+const tea= ()=>{ 
         const Amep = this.state.easyA.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.correct_answer}{who.incorrect_answers} </div>
+                   {/* <Questions QAE={who.question} AAE={who.correct_answer} IAE={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
@@ -100,8 +146,7 @@ class Api extends Component {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    //   <Questions QAM={who.question} ABM={who.correct_answer} IBM={who.incorrect_answers}></Questions>
                 </div>
 
             )
@@ -110,187 +155,161 @@ class Api extends Component {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                      {/* <Questions QAH={who.question} AAH={who.correct_answer} IAH={who.incorrect_answers} CA={who.category}></Questions> */}
                 </div>
 
             )
         })
-        const Bmep = this.state.easyA.map((who, ) => {
+        const Bmep = this.state.easyB.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.correct_answer}{who.incorrect_answers} </div>
+                       {/* <Questions QBE={who.question} ABE={who.correct_answer} IBE={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Bbeep = this.state.mediumA.map((who, ) => {
+        const Bbeep = this.state.mediumB.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                   {/* <Questions QBM={who.question} ABM={who.correct_answer} IBM={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Bceeep = this.state.hardA.map((who, ) => {
+        const Bceeep = this.state.hardB.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                     {/* <Questions QBH={who.question} ABH={who.correct_answer} IBH={who.incorrect_answers} CB={who.category}></Questions> */}
                 </div>
 
             )
         })
-        const Cmep = this.state.easyA.map((who, ) => {
+        const Cmep = this.state.easyC.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.correct_answer}{who.incorrect_answers} </div>
+                      {/* <Questions QCE={who.question} ACE={who.correct_answer} ICE={who.incorrect_answers} CC={who.category}></Questions> */}
                 </div>
 
             )
         })
-        const Cbeep = this.state.mediumA.map((who, ) => {
+        const Cbeep = this.state.mediumC.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                      {/* <Questions QCM={who.question} ACM={who.correct_answer} ICM={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Cceeep = this.state.hardA.map((who, ) => {
+        const Cceeep = this.state.hardC.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    {/* <Questions QCH={who.question} ACH={who.correct_answer} ICH={who.incorrect_answers} CC={who.category}></Questions> */}
                 </div>
 
             )
         })
-        const Dmep = this.state.easyA.map((who, ) => {
+        const Dmep = this.state.easyD.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    {/* <Questions QDE={who.question} ADE={who.correct_answer} IDE={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Dbeep = this.state.mediumA.map((who, ) => {
+        const Dbeep = this.state.mediumD.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    {/* <Questions QDM={who.question} ADM={who.correct_answer} IDM={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Dceeep = this.state.hardA.map((who, ) => {
+        const Dceeep = this.state.hardD.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                      {/* <Questions QDH={who.question} ADH={who.correct_answer} IDH={who.incorrect_answers} CD={who.category}></Questions> */}
                 </div>
 
             )
         })
-        const Emep = this.state.easyA.map((who, ) => {
+        const Emep = this.state.easyE.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.correct_answer}{who.incorrect_answers} </div>
+                     {/* <Questions QEE={who.question} AEE={who.correct_answer} IEE={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Ebeep = this.state.mediumA.map((who, ) => {
+        const Ebeep = this.state.mediumE.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                     {/* <Questions QEM={who.question} AEM={who.correct_answer} IEM={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Eceeep = this.state.hardA.map((who, ) => {
+        const Eceeep = this.state.hardE.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    {/* <Questions QEH={who.question} AEH={who.correct_answer} IEH={who.incorrect_answers} CE={who.category}></Questions> */}
                 </div>
 
             )
         })
-        const Fmep = this.state.easyA.map((who, ) => {
+        const Fmep = this.state.easyF.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.correct_answer}{who.incorrect_answers} </div>
+                     {/* <Questions QFE={who.question} AFE={who.correct_answer} IFE={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Fbeep = this.state.mediumA.map((who, ) => {
+        const Fbeep = this.state.mediumF.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                       {/* <Questions QFM={who.question} AFM={who.correct_answer} IFM={who.incorrect_answers}></Questions> */}
                 </div>
 
             )
         })
-        const Fceeep = this.state.hardA.map((who, ) => {
+        const Fceeep = this.state.hardF.map((who, ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
-                    <div>{who.incorrect_answers}{who.correct_answer} </div>
+                      {/* <Questions QFH={who.question} AFH={who.correct_answer} IFH={who.incorrect_answers} CF={who.category}></Questions> */}
                 </div>
 
             )
         })
-        
+
+        const copy=[Fceeep,Fbeep,Fmep,Eceeep,Ebeep,Emep,Dceeep,Dbeep,Dmep,Cceeep,Cbeep,Cmep,Bceeep,Bbeep,Bmep,Aceeep,Abeep,Amep]
+        return(
+            <Questions copy={copy}  />
+        )
+    }
 
         return (
             <div>
 
-                <div>
+           
                     
-                    {Amep} {Abeep}{Aceeep}
-                </div>
-                <div>
-                    {Bmep} {Bbeep}{Bceeep}
-                </div>
-                <div>
-                    {Cmep} {Cbeep}{Cceeep}
-                </div>
-                <div>
-                    {Dmep} {Dbeep}{Dceeep}
-                </div>
-                <div>
-                    {Emep} {Ebeep}{Eceeep}
-                </div>
-                <div>
-                    {Fmep} {Fbeep}{Fceeep}
-                    
-                </div>
+                 {tea}
+               
+                
             </div>
 
         );
