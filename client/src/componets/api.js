442aa32d762f0c5abb18359a71221695c3181d13
kennf136 <kennf136@gmail.com>
2018-08-10 22:18:20 -0400

    Points on borad, this project looks like the first, sadface

--- client/src/componets/api.js
+++ client/src/componets/api.js
@@ -26,7 +26,7 @@ class Api extends Component {
         easyF: [{}],
         mediumF: [{}],
         hardF: [{}],
-
+       show:false
 
     }
         ;
@@ -132,182 +132,200 @@ class Api extends Component {
             )
         })
 
-        const Amep = this.state.easyB.map((who, ) => {
+        const Amep = this.state.easyB.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+1)*200} </div>
                 </div>
 
             )
         })
-        const Abeep = this.state.mediumB.map((who, ) => {
+        const Abeep = this.state.mediumB.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+3)*200} </div>
                 </div>
 
             )
         })
-        const Aceeep = this.state.hardB.map((who, ) => {
+        const Aceeep = this.state.hardB.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+5)*200} </div>
                 </div>
 
             )
         })
-        const Bmep = this.state.easyC.map((who, ) => {
+        const Bmep = this.state.easyC.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+1)*200} </div>
                 </div>
 
             )
         })
-        const Bbeep = this.state.mediumC.map((who, ) => {
+        const Bbeep = this.state.mediumC.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+3)*200} </div>
                 </div>
 
             )
         })
-        const Bceeep = this.state.hardC.map((who, ) => {
+        const Bceeep = this.state.hardC.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+5)*200} </div>
                 </div>
 
             )
         })
-        const Cmep = this.state.easyD.map((who, ) => {
+        const Cmep = this.state.easyD.map((who, i) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+1)*200} </div>
                 </div>
 
             )
         })
-        const Cbeep = this.state.mediumD.map((who, ) => {
+        const Cbeep = this.state.mediumD.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+3)*200} </div>
                 </div>
 
             )
         })
-        const Cceeep = this.state.hardD.map((who, ) => {
+        const Cceeep = this.state.hardD.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+5)*200} </div>
                 </div>
 
             )
         })
-        const Dmep = this.state.easyE.map((who, ) => {
+        const Dmep = this.state.easyE.map((who, i) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+1)*200} </div>
                 </div>
 
             )
         })
-        const Dbeep = this.state.mediumE.map((who, ) => {
+        const Dbeep = this.state.mediumE.map((who, i) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+3)*200} </div>
                 </div>
 
             )
         })
-        const Dceeep = this.state.hardE.map((who, ) => {
+        const Dceeep = this.state.hardE.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+5)*200} </div>
                 </div>
 
             )
         })
-        const Emep = this.state.easyE.map((who, ) => {
+        const Emep = this.state.easyE.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+1)*200} </div>
                 </div>
 
             )
         })
-        const Ebeep = this.state.mediumE.map((who, ) => {
+        const Ebeep = this.state.mediumE.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+3)*200} </div>
                 </div>
 
             )
         })
-        const Eceeep = this.state.hardE.map((who, ) => {
+        const Eceeep = this.state.hardE.map((who,i ) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+5)*200} </div>
                 </div>
 
             )
         })
-        const Fmep = this.state.easyF.map((who, ) => {
+        const Fmep = this.state.easyF.map((who, i) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.correct_answer}{who.incorrect_answers} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+1)*200} </div>
                 </div>
 
             )
         })
-        const Fbeep = this.state.mediumF.map((who, ) => {
+        const Fbeep = this.state.mediumF.map((who, i) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+3)*200} </div>
                 </div>
 
             )
         })
-        const Fceeep = this.state.hardF.map((who, ) => {
+        const Fceeep = this.state.hardF.map((who, i) => {
             return (
                 <div>
 
-                    <div> {who.question}  </div>
+                    <h3> {who.question}  </h3>
                     <div>{who.incorrect_answers}{who.correct_answer} </div>
+                    <div onClick={()=>this.setState({show:true})}> $ {(i+5)*200} </div>
                 </div>
 
             )
