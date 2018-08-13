d11e201477d7100130d64238b2954b7f8c9a60bd
kennf136 <kennf136@gmail.com>
2018-08-13 15:15:59 -0400

    display

--- client/src/componets/SignIn.js
+++ client/src/componets/SignIn.js
@@ -4,7 +4,8 @@ class SignIn extends Component {
     render() {
         return (
             <div>
-                
+                <div> game one </div>
+                <div> game two </div>
             </div>
         );
     }
